40a1250d90c0fc3a94844695cfd0a41a45a86e2a
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-06-01 16:11:51 -0300

    feat: Import PrismaModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ExperienceModule } from './experience/experience.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ExperienceModule } from "./experience/experience.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [ExperienceModule],
+  imports: [ExperienceModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
