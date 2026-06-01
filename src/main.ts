99a1e52d84dabfe0c6f61d576e7e4af566ad5392
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-06-01 15:54:42 -0300

    feat: Use pipe validation of zod in all aplication

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+} from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
+import { ZodValidationPipe } from "nestjs-zod";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,7 +14,9 @@ async function bootstrap() {
 
   app.enableCors();
 
-  await app.listen(3001, '0.0.0.0');
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  await app.listen(3001, "0.0.0.0");
   console.log(`🚀 Backend rodando na porta 3001 com Fastify`);
 }
 bootstrap();
