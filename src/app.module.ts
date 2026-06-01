b2c866a75e500e4922c5188c147d5adc8be66bd6
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-06-01 16:13:03 -0300

    feat: Generate module history

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ExperienceModule } from "./experience/experience.module";
 import { PrismaModule } from "./prisma/prisma.module";
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [ExperienceModule, PrismaModule],
+  imports: [ExperienceModule, PrismaModule, HistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
