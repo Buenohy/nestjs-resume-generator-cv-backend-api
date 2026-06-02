a1926692a012a1c1bea2649cd090e77cb2bfba4a
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-06-02 12:37:56 -0300

    feat: Ajust imports prisma/client

--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { CreateHistoryDto } from "./dto/create-history.dto";
-import { Prisma } from "../generated/prisma/client";
+import { Prisma } from "@prisma/client";
 
 @Injectable()
 export class HistoryService {
