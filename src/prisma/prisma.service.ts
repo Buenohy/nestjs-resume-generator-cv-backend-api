e44cc5ae35b9221f1724aef6f80b7837dc85fbb6
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-06-02 12:49:53 -0300

    feat: Import doenv

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
@@ -10,10 +11,12 @@ export class PrismaService
 {
   constructor() {
     const connectionString = process.env.DATABASE_URL;
+
+    // Cria o pool de conexões passando as credenciais carregadas do .env
     const pool = new Pool({ connectionString });
     const adapter = new PrismaPg(pool);
 
-    // Inicializa o Prisma Client v7 com o adaptador do Postgres
+    // Inicializa o Prisma Client v7 com o adaptador seguro do Postgres
     super({ adapter });
   }
 
